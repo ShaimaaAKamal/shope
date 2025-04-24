e82e264a9a9b6a4e721963c0c64f3e727a168466
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-24 02:20:03 +0200

    import feature modules and add it to to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule) },
+  { path: 'orders', loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersModule) },
+  { path: 'orders', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
