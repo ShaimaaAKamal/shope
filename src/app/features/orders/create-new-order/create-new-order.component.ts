7dba60d9bf091fa109dc6936f11cfaf5c8a14f51
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-25 19:39:09 +0300

    update create component by organizing code and use order details

--- src/app/features/orders/create-new-order/create-new-order.component.ts
+++ src/app/features/orders/create-new-order/create-new-order.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../../../Interfaces/product';
 import { CustomerService } from '../../../Services/Customer/customer.service';
 import { ProductService } from '../../../Services/Product/product.service';
 import { OrderService } from '../../../Services/order/order.service';
+import { Order } from '../../../Interfaces/order';
 
 @Component({
   selector: 'app-create-new-order',
@@ -13,53 +14,12 @@ import { OrderService } from '../../../Services/order/order.service';
   styleUrl: './create-new-order.component.scss'
 })
 export class CreateNewOrderComponent {
-@ViewChild('searchInputByName') searchInputByName!: InputComponent;
-@ViewChild('searchInputByBarcode') searchInputByBarcode!: InputComponent;
-
-customers!:Customer[];
-selectedCustomer:Customer={name:'' , id:-1};
-searchItems:Product[]=[];
-
-constructor(private __CustomerService:CustomerService
-  ,private __ProductsService:ProductService
-  ,private __OrderService:OrderService){}
-
+  
+constructor(private __OrderService:OrderService){}
 ngOnInit(): void {
- this.customers=this.__CustomerService.customers();
-}
-selectedCustomerEvent(customer:Customer){
-  this.selectedCustomer=customer;
-  this.__OrderService.setInvoiveCustomer(customer);
-}
-searchProductsByName($event:any){
-const searchKey=$event.target.value;
- this.searchItems=this.__ProductsService.findProductByName(searchKey);
-}
-
-searchProductsByBarcode($event:any){
-  const searchKey=$event.target.value;
-  const filteredProducts:Product[]=this.__ProductsService.findProductByBarcode(searchKey);
-  if(filteredProducts.length>0)
-  {  this.addProductItem(filteredProducts[0]);
-    this.searchInputByBarcode.value='';
-  }
-}
-selectItem(product:Product){
-    this.addProductItem(product);
-    this.searchInputByName.value = '';
-    this.searchItems=[];
-}
-
-private addProductItem(proudct:Product){
-let products:Product[]=this.__OrderService.orderProducts();
- let existingProductIndex:number = products.findIndex(p => p.id === proudct.id);
-if (existingProductIndex > -1) {
-  products[existingProductIndex].quantity =( Number(products[existingProductIndex].quantity) +1).toString();
-} else {
-  proudct.quantity = '1';
-  products.push(proudct);
-}
-    this.__OrderService.UpdateProducts(products);
+  this.__OrderService.orderProducts.set([]);
+  this.__OrderService.discount.set(0);
+  this.__OrderService.setPaidAmount(0);
 
 }
 }
