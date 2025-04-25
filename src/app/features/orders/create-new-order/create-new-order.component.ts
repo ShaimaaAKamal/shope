984e4fe2a62c32895755d3593cff45fe2b0a323d
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-25 19:42:21 +0300

    remove unused imports

--- src/app/features/orders/create-new-order/create-new-order.component.ts
+++ src/app/features/orders/create-new-order/create-new-order.component.ts
@@ -1,11 +1,5 @@
-import { Component, ViewChild } from '@angular/core';
-import { InputComponent } from '../../../shared/components/input/input.component';
-import { Customer } from '../../../Interfaces/customer';
-import { Product } from '../../../Interfaces/product';
-import { CustomerService } from '../../../Services/Customer/customer.service';
-import { ProductService } from '../../../Services/Product/product.service';
+import { Component } from '@angular/core';
 import { OrderService } from '../../../Services/order/order.service';
-import { Order } from '../../../Interfaces/order';
 
 @Component({
   selector: 'app-create-new-order',
@@ -14,7 +8,7 @@ import { Order } from '../../../Interfaces/order';
   styleUrl: './create-new-order.component.scss'
 })
 export class CreateNewOrderComponent {
-  
+
 constructor(private __OrderService:OrderService){}
 ngOnInit(): void {
   this.__OrderService.orderProducts.set([]);
