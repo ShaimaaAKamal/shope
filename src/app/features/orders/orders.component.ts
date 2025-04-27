7e5c01ca53f073e1d54f6a0fc3469cb5690b8cd3
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-28 00:26:00 +0300

    initiate orders component

--- src/app/features/orders/orders.component.ts
+++ src/app/features/orders/orders.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { OrderService } from '../../Services/order/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './orders.component.scss'
 })
 export class OrdersComponent {
+ __OrderService=inject(OrderService);
+__Router=inject(Router);
 
+ orders=this.__OrderService.orders;
+ handleDispalyedItems(event:any){}
+ addNew(event:any){
+  this.__Router.navigateByUrl('Orders/create');
+ }
+ handleDelete(event:any){}
 }
