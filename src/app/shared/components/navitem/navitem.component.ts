c6aac5f466dcea186092a523c099879172d0e24f
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-27 22:49:10 +0300

    update navitem
    to handle different link click behaviour

--- src/app/shared/components/navitem/navitem.component.ts
+++ src/app/shared/components/navitem/navitem.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navitem',
@@ -12,10 +13,30 @@ export class NavitemComponent {
 @Input() classes:string='';
 @ViewChild('shopping') shopping!:ElementRef;
 
+constructor(private __Router:Router){}
 showHeader(){
   this.shopping.nativeElement.classList.remove('d-none');
 }
 hideHeader(){
   this.shopping.nativeElement.classList.add('d-none');
+}
+
+handleSpecialClick(name: string) {
+  switch (name) {
+    case 'Hold_Orders':
+      console.log('Hold Orders clicked!');
+      this.__Router.navigateByUrl('Orders/create?popup=hold_orders');
+      break;
+    case 'Return_Order':
+      console.log('Return Order clicked!');
+      // your return_order logic here
+      break;
+    case 'End_Day':
+      console.log('End Day clicked!');
+      // your end_Day logic here
+      break;
+    default:
+      break;
+  }
 }
 }
