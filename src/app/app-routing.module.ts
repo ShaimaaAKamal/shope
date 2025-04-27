a5bb503959de7e4672984d56d6c65c8d73f8874f
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-27 22:49:45 +0300

    Update app-routing.module.ts
    adding 404 route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo:"Orders/create" ,pathMatch:'full'},
   { path: 'Home', redirectTo:"Orders/create" },
   { path: 'Products', loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule) },
   { path: 'Orders', loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersModule) },
-  { path: 'Customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) }];
+  { path: 'Customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) },
+  {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
