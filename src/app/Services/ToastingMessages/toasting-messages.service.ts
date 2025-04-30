cde0ef8f41d31c827bc97f2a2b95769419502ac3
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-30 19:58:46 +0300

    translate toast messages

--- src/app/Services/ToastingMessages/toasting-messages.service.ts
+++ src/app/Services/ToastingMessages/toasting-messages.service.ts
@@ -1,28 +1,34 @@
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToastingMessagesService {
 
-  constructor(private _ToastrService:ToastrService) { }
+  constructor(
+    private _ToastrService: ToastrService,
+    private translateService: TranslateService
+  ) {}
 
-  showToast(message: string, type: 'success' | 'error' | 'info' | 'warning') {
+  showToast(toastMessage: string, type: 'success' | 'error' | 'info' | 'warning') {
+    const titleKey = `TOAST.${type.toUpperCase()}`;
+    const translatedTitle = this.translateService.instant(titleKey);
+    const message=this.translateService.instant(toastMessage);
     switch (type) {
       case 'success':
-        this._ToastrService.success(message, 'Success');
+        this._ToastrService.success(message, translatedTitle);
         break;
       case 'error':
-        this._ToastrService.error(message, 'Error');
+        this._ToastrService.error(message, translatedTitle);
         break;
       case 'info':
-        this._ToastrService.info(message, 'Info');
+        this._ToastrService.info(message, translatedTitle);
         break;
       case 'warning':
-        this._ToastrService.warning(message, 'Warning');
+        this._ToastrService.warning(message, translatedTitle);
         break;
     }
   }
-
 }
