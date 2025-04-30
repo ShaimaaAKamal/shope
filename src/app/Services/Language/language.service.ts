e08a1f6d75a540990f089ef3d97f6668722bc28d
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-04-30 06:39:27 +0300

    Update language.service.ts
    update this to presist lang settings while reload

--- src/app/Services/Language/language.service.ts
+++ src/app/Services/Language/language.service.ts
@@ -1,5 +1,6 @@
-import { computed, Injectable, Signal, signal } from '@angular/core';
+import { computed, effect, Injectable, Signal, signal } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { CommonService } from '../CommonService/common.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,23 @@ export class LanguageService {
   dirSignal: Signal<string>;
   rtlClassSignal: Signal<boolean>;
 
-  constructor(private translateService: TranslateService) {
-    this.translateService.setDefaultLang('en');
-    this.translateService.use('en');
+  constructor(private translateService: TranslateService,private __CommonService:CommonService) {
+    const lang=this.__CommonService.getItemsFromStorage<string>('lang','en');
+    if(lang != 'en') this.langSignal.set(lang);
+    this.translateService.setDefaultLang(lang);
+    this.translateService.use(lang);
      this.dirSignal = computed(() => {
       const lang = this.langSignal();
       return lang === 'ar' ? 'rtl' : 'ltr';
     });
 
     this.rtlClassSignal = computed(() => this.langSignal() === 'ar');
+
+     effect(() => {
+    const lang = this.langSignal();
+    const isRtl = this.rtlClassSignal();
+    this.switchStyleToRTL(isRtl, lang);
+  });
   }
 
   getLangSignal(): Signal<string> {
@@ -28,9 +37,9 @@ export class LanguageService {
   changeLanguage(lang: string): void {
     this.langSignal.set(lang);
     this.translateService.use(lang);
-    this.switchStyleToRTL(this.rtlClassSignal(),lang);
+    this.__CommonService.saveToStorage('lang',lang)
   }
-    switchStyleToRTL(isRTL: boolean,lang:string) {
+  switchStyleToRTL(isRTL: boolean,lang:string) {
     const head = document.getElementsByTagName('head')[0];
     let existingLink = document.getElementById('bootstrap-style') as HTMLLinkElement;
     if (existingLink) {
