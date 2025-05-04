0140d21fe5d623e56e9dd2394a32e35350a11873
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-05-04 21:34:28 +0300

    Update orders-routing.module.ts
    add return order route

--- src/app/features/orders/orders-routing.module.ts
+++ src/app/features/orders/orders-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { OrdersComponent } from './orders.component';
 import { CreateNewOrderComponent } from './create-new-order/create-new-order.component';
 import { OrderComponent } from './order/order.component';
+import { ReturnOrderComponent } from './return-order/return-order.component';
 
 const routes: Routes = [
   { path: '', component: OrdersComponent  },
   { path: 'create', component: CreateNewOrderComponent },
+  { path: 'Return_Order', component: ReturnOrderComponent },
+  // { path: ':id', component: OrderPageComponent },
   { path: 'Order/:code', component: OrderComponent }
 
 ];
