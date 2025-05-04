1e3118504104976c88c164919b0bee94157d44d1
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-05-05 02:01:34 +0300

    Update customer.service.ts
    add new functionality in service to add new customer

--- src/app/Services/Customer/customer.service.ts
+++ src/app/Services/Customer/customer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 import { Customer } from '../../Interfaces/customer';
+import { CommonService } from '../CommonService/common.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,22 @@ export class CustomerService {
    { name:"Ali Muhamed",nameAr:'علي محمد',id:2,phone:"01020432563"},
     {name:"Omar Ali",nameAr:'عمر علي',id:3,phone:"01020432563"}
   ]);
-  constructor() { }
+  constructor(private __CommonService:CommonService) {
+      this.customers.set(this.__CommonService.getItemsFromStorage<  Customer[]>('customers',[]));
+   }
+
+
+  addCustomer(customer:Customer){
+     let result=this.__CommonService.findItemInArray(this.customers(),c => c.phone == customer.phone);
+    if(result.exists) return {status:false , message : 'This Customer is already exist!'};
+    else{
+        this.setCustomers([...this.customers(),customer]);
+        return {status:true , message : 'customer has been added successfully!'};
+    }
+  }
+
+    setCustomers(customers:Customer[]){
+        this.customers.set(customers);
+        this.__CommonService.saveToStorage('customers',customers);
+    }
 }
