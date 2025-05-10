5ad8ba69b5b9324874b256611ce3224ed89c9c6b
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-05-11 00:44:13 +0300

    finish sidenav create variant

---
+++ src/app/features/products/create-variant/create-variant.component.ts
@@ -0,0 +1,100 @@
+import { Component, EventEmitter, inject, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { ProductService } from '../../../Services/Product/product.service';
+import { InputComponent } from '../../../shared/components/input/input.component';
+import { CommonService } from '../../../Services/CommonService/common.service';
+
+@Component({
+  selector: 'app-create-variant',
+  standalone: false,
+  templateUrl: './create-variant.component.html',
+  styleUrl: './create-variant.component.scss'
+})
+export class CreateVariantComponent {
+  private __CommonService=inject(CommonService);
+  private __ProductService=inject(ProductService);
+  variantOptions=this.__ProductService.variantOptions;
+
+  nameErrorMessage:string='';
+  arabicNameErrorMessage:string='';
+
+  values: any[] =  [];
+
+ @ViewChild('optionName') optionName!:InputComponent ;
+@ViewChild('optionArabicName') optionArabicName!:InputComponent ;
+
+@ViewChildren('variantValue') variantValuesRefs!: QueryList<InputComponent>;
+@Output() closeAddVariantPopScreen=new EventEmitter<void>();
+addNewValue() {
+    this.values.push({id: crypto.randomUUID(),value:'',color:'#000'});
+  }
+
+removeValue(id: string): void {
+  this.values = this.values.filter(v => v.id !== id);
+}
+
+add() {
+  const variantValues = this.getUniqueTrimmedValues();
+  const variantName = this.optionName.value?.trim();
+  const variantArabicName = this.optionArabicName.value?.trim();
+
+  if (!this.validateNames(variantName, variantArabicName)) return;
+
+  if (this.isDuplicateVariant(variantName, variantArabicName)) {
+    this.nameErrorMessage = 'Variant already exists';
+    return;
+  }
+
+   if (variantValues.length === 0) {
+    this.nameErrorMessage = 'At least one value is required';
+    return;
+  }
+
+  const newVariant = {
+    id: this.variantOptions().length + 1,
+    name: variantName,
+    nameAr: variantArabicName,
+    values: variantValues
+  };
+
+  this.__ProductService.updateVariants(newVariant);
+  this.closeAddVariantPopScreen.emit();
+}
+private getUniqueTrimmedValues(): string[] {
+  return Array.from(new Set(
+    this.variantValuesRefs
+      .map(input => input.value?.trim())
+      .filter(Boolean)
+  ));
+}
+
+private validateNames(name: string, arabicName: string): boolean {
+  this.nameErrorMessage = '';
+  this.arabicNameErrorMessage = '';
+
+  let isValid = true;
+
+  if (!name) {
+    this.nameErrorMessage = 'Name is required';
+    isValid = false;
+  }
+
+  if (!arabicName) {
+    this.arabicNameErrorMessage = 'Arabic Name is required';
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+private isDuplicateVariant(name: string, arabicName: string): boolean {
+  const variants = this.variantOptions();
+  const result = this.__CommonService.findItemInArray(
+    variants,
+    v => v.name === name || v.nameAr === arabicName
+  );
+  return result.exists;
+}
+  close(){
+     this.closeAddVariantPopScreen.emit();
+  }
+}
