a523228d43fc51efc8cfc8b6c9d7cb1bff066104
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-05-12 10:25:43 +0300

    update sales person into one input
    update all components that related on it

--- src/app/shared/components/nav-sales-person/nav-sales-person.component.ts
+++ src/app/shared/components/nav-sales-person/nav-sales-person.component.ts
@@ -1,5 +1,10 @@
 import { Component, Input } from '@angular/core';
 
+interface NavSalesPersonInterface{
+  isCollapsed:boolean,
+  personImage:string,
+  salesPersonName:string
+}
 @Component({
   selector: 'app-nav-sales-person',
   standalone: false,
@@ -7,7 +12,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './nav-sales-person.component.scss'
 })
 export class NavSalesPersonComponent {
-@Input() isCollapsed:boolean=false;
-@Input() personImage:string='';
-@Input() salesPersonName:string='';
+@Input() NavSalesPersonComponent:NavSalesPersonInterface={
+  isCollapsed:false,
+  personImage:'',
+  salesPersonName:''
+}
+
 }
