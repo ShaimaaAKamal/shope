6711954be4799db896188ae445512fe5e59f27ce
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-06-17 16:23:42 +0300

    update create variant to variant library item

--- src/app/features/products/components/create-variant/create-variant.component.ts
+++ src/app/features/products/components/create-variant/create-variant.component.ts
@@ -1,10 +1,4 @@
-import { Component, EventEmitter, inject, Input, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { ProductService } from '../../../../Services/Product/product.service';
-import { InputComponent } from '../../../../shared/components/input/input.component';
-import { CommonService } from '../../../../Services/CommonService/common.service';
-import { ToastingMessagesService } from '../../../../Services/ToastingMessages/toasting-messages.service';
-import { VariantOption } from '../../../../Interfaces/variant-option';
-
+import {Component, EventEmitter,Output, signal} from '@angular/core';
 @Component({
   selector: 'app-create-variant',
   standalone: false,
@@ -12,93 +6,15 @@ import { VariantOption } from '../../../../Interfaces/variant-option';
   styleUrl: './create-variant.component.scss'
 })
 export class CreateVariantComponent {
-  private __CommonService=inject(CommonService);
-  private __ProductService=inject(ProductService);
-  private __ToastingMessagesService=inject(ToastingMessagesService);
 
-  variantOptions=this.__ProductService.variantOptions;
+  @Output() closeAddVariantPopScreen = new EventEmitter<void>();
+  action=signal<string>('');
 
-  nameErrorMessage:string='';
-  arabicNameErrorMessage:string='';
-
-  values: any[] =  [];
-
-@ViewChild('optionName') optionName!:InputComponent ;
-@ViewChild('optionArabicName') optionArabicName!:InputComponent ;
-
-@ViewChildren('variantValue') variantValuesRefs!: QueryList<InputComponent>;
-@Output() closeAddVariantPopScreen=new EventEmitter<void>();
-
-addNewValue() {
-    this.values.push({id: crypto.randomUUID(),value:'',color:'#000'});
+  add() {
+    this.action.set('add');
   }
 
-removeValue(id: string): void {
-  this.values = this.values.filter(v => v.id !== id);
-}
-
-async add() {
-  const variantValues = this.getUniqueTrimmedValues();
-  const variantName = this.optionName.value?.trim();
-  const variantArabicName = this.optionArabicName.value?.trim();
-
-  if (!this.validateNames(variantName, variantArabicName)) return;
-
-  if (this.isDuplicateVariant(variantName, variantArabicName)) {
-    this.nameErrorMessage = 'Variant already exists';
-    return;
-  }
-
-   if (variantValues.length === 0) {
-    this.nameErrorMessage = 'At least one value is required';
-    return;
-  }
-
-  const newVariant = {
-    id: this.__CommonService.getId(),
-    name: variantName,
-    nameAr: variantArabicName,
-    values: variantValues
-  };
-  this.__ProductService.createVariant(newVariant).subscribe({
-    next:()=>this.closeAddVariantPopScreen.emit()
-  })}
-private getUniqueTrimmedValues(): string[] {
-  return Array.from(new Set(
-    this.variantValuesRefs
-      .map(input => input.value?.trim())
-      .filter(Boolean)
-  ));
-}
-
-private validateNames(name: string, arabicName: string): boolean {
-  this.nameErrorMessage = '';
-  this.arabicNameErrorMessage = '';
-
-  let isValid = true;
-
-  if (!name) {
-    this.nameErrorMessage = 'Name is required';
-    isValid = false;
-  }
-
-  if (!arabicName) {
-    this.arabicNameErrorMessage = 'Arabic Name is required';
-    isValid = false;
-  }
-
-  return isValid;
-}
-
-private isDuplicateVariant(name: string, arabicName: string): boolean {
-  const variants = this.variantOptions();
-  const result = this.__CommonService.findItemInArray(
-    variants,
-    v => v.name === name || v.nameAr === arabicName
-  );
-  return result.exists;
-}
-  close(){
-     this.closeAddVariantPopScreen.emit();
+  close() {
+    this.closeAddVariantPopScreen.emit();
   }
 }
