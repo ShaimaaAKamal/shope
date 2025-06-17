0dad1958bedc538fa12fd17fb3c8e17ab8b2977c
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-06-17 21:59:53 +0300

    update edit variant to use variant library component

--- src/app/features/products/edit-variant/edit-variant.component.ts
+++ src/app/features/products/edit-variant/edit-variant.component.ts
@@ -1,10 +1,5 @@
-import {Component,Input,QueryList,signal,ViewChildren,OnDestroy,inject,Output,EventEmitter,ViewChild,SimpleChanges} from '@angular/core';
-import { Subscription } from 'rxjs';
-import { VariantOption } from '../../../Interfaces/variant-option';
-import { InputComponent } from '../../../shared/components/input/input.component';
-import { ProductService } from '../../../Services/Product/product.service';
-import { ToastingMessagesService } from '../../../Services/ToastingMessages/toasting-messages.service';
-import { _ } from '@ngx-translate/core';
+import {Component,Input,signal,Output,EventEmitter} from '@angular/core';
+import { VariantMasterLookUP } from '../../../Interfaces/variant-master-look-up';
 
 @Component({
   selector: 'app-edit-variant',
@@ -12,121 +7,19 @@ import { _ } from '@ngx-translate/core';
   templateUrl: './edit-variant.component.html',
   styleUrl: './edit-variant.component.scss'
 })
-export class EditVariantComponent implements OnDestroy {
-   private __ProductService=inject(ProductService);
-  private __ToastingMessagesService=inject(ToastingMessagesService);
-  private changesSub?: Subscription;
+export class EditVariantComponent {
+    action=signal<string>('');
 
-  @Input() variant!: VariantOption;
+  @Input() variant!: VariantMasterLookUP;
+
   @Output() updated=new EventEmitter<void>();
-  @ViewChildren('variantValue') variantValues!: QueryList<InputComponent>;
-  @ViewChildren('colorPick') colorPicks!: QueryList<InputComponent>;
-  @ViewChild('name') variantName!:InputComponent;
-  @ViewChild('nameAr') variantArabicName!:InputComponent;
 
-
-  values = signal<string[]>([]);
-
-  insertVariantNewValue = false;
-  showColorPickerContainer =signal<boolean>(false);
-  color = '#000';
-  viewInitialized:boolean=false;
-  errorMessage='';
-
-displayVariantValues(){
-      const currentValues = this.values();
-      if(this.variant.name == 'color' || this.variant.nameAr == 'اللون'){
-         this.showColorPickerContainer.set(true);
-      }
-      else
-      this.variantValues?.forEach((inputComponent, index) => {
-        inputComponent.value = currentValues[index];
-      });
-  }
-
-displayVariantBasicInfo(){
-    this.values.set(this.variant.values);
-    this.variantName.value=this.variant.name;
-    this.variantArabicName.value=this.variant.nameAr
-  }
-ngOnChanges(changes: SimpleChanges): void {
-  if (changes['variant'] && changes['variant'].currentValue) {
-
-    if (this.viewInitialized) {
-          this.displayVariantBasicInfo();
-    this.resetColorPicker();
-    this.displayVariantValues();
-    }
-  }
+updatedVariant(){
+  this.updated.emit();
 }
-
-ngAfterViewInit(): void {
-    this.viewInitialized=true;
-      this.displayVariantBasicInfo();
-      this.changesSub = this.variantValues.changes.subscribe(() => {
-        this.displayVariantValues();
-      });
-  }
-
-  resetColorPicker(){
-        this.insertVariantNewValue=false;
-        if(this.variant.name != 'color' )
-    {   this.showColorPickerContainer.set(false)}
-  }
-
-  removeValue(index: number): void {
-  const updated = [...this.values()]
-  updated.splice(index, 1);
-  this.values.set(updated);
-  }
-
-  showNewValueInput() {
-    const newValue=(this.variant.name == 'color') ? '#000' : '';
-    this.values.update(current => [...current,newValue])
-    this.insertVariantNewValue=true;
-    this.errorMessage='';
-  }
-
-  addNewValue() {
-  this.errorMessage = '';
-
-  if (this.variant.name !== 'color') {
-    const currentValues = this.variantValues?.map(vc => vc.value.trim()) || [];
-    const uniqueValues = Array.from(new Set(currentValues.filter(v => v !== '')));
-
-    if (uniqueValues.length < currentValues.length) {
-      this.errorMessage = 'This New added Value is already existed';
-    }
-
-    this.values.set(uniqueValues);
-  } else {
-    const allValues = this.values().map(v => v.trim());
-    const uniqueValues = Array.from(new Set(allValues));
-
-    if (uniqueValues.length < allValues.length) {
-      this.errorMessage = 'This New added Value is already existed';
-    }
-
-    this.values.set(uniqueValues);
-  }
-
-  this.insertVariantNewValue = false;
-}
-
 updateVariant(){
-const newVariant={
-  id:this.variant.id,
-  name:this.variantName.value.trim(),
-  nameAr:this.variantArabicName.value.trim(),
-  values:[...new Set(this.values().filter(v => v.trim() !== ''))]
-}
-this.__ProductService.updateVariant(newVariant).subscribe({
-  next:()=>{this.errorMessage='';this.updated.emit();},
-  error:(err)=>    this.errorMessage=err.message
-})
+  this.action.set('update');
   }
 
-ngOnDestroy(): void {
-    this.changesSub?.unsubscribe();
-  }
+
 }
