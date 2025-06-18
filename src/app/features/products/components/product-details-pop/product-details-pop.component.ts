b4dd9ec5d4c5e214c8f38d8d374af3200be74032
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-06-18 20:26:28 +0300

    update product details to match new product implementation

--- src/app/features/products/components/product-details-pop/product-details-pop.component.ts
+++ src/app/features/products/components/product-details-pop/product-details-pop.component.ts
@@ -1,6 +1,7 @@
 import {  Component, effect, EventEmitter, inject, Output, ViewChild } from '@angular/core';
 import { InputComponent } from '../../../../shared/components/input/input.component';
 import { ProductService } from '../../../../Services/Product/product.service';
+import { Product } from '../../../../Interfaces/product';
 
 @Component({
   selector: 'app-product-details-pop',
@@ -10,28 +11,51 @@ import { ProductService } from '../../../../Services/Product/product.service';
 })
 export class ProductDetailsPopComponent {
   // ViewChild references
-@ViewChild('productWeight') productWeight!:InputComponent;
-@ViewChild('productCostPrice') productCostPrice!:InputComponent;
-@ViewChild('salePrice') salePrice!:InputComponent;
-@ViewChild('salePriceEndDate') salePriceEndDate!:InputComponent;
+// @ViewChild('productWeight') productWeight!:InputComponent;
+// @ViewChild('salePrice') salePrice!:InputComponent;
+// @ViewChild('salePriceEndDate') salePriceEndDate!:InputComponent;
+// @ViewChild('productCostPrice') productCostPrice!:InputComponent;
+@ViewChild('descriptionEn') descriptionEn!:InputComponent;
+@ViewChild('descriptionAr') descriptionAr!:InputComponent;
+@ViewChild('productComparePrice') productComparePrice!:InputComponent;
+@ViewChild('tax') tax!:InputComponent;
+@ViewChild('vatValue') vatValue!:InputComponent;
 @ViewChild('barcode') barcode!:InputComponent;
+@ViewChild('SKU') SKU!:InputComponent;
 
     // Outputs
 @Output() updated=new EventEmitter<boolean>();
-// @Output() close=new EventEmitter<boolean>();
-
+@Output() close=new EventEmitter<boolean>();
+// chargeTax:boolean=false
+// isActive:boolean=true;
+// isPriceAffecting:boolean=false;
 private __ProductService=inject(ProductService);
 product=this.__ProductService.currentProduct;
-weightDropDownSelection:string='KG';
-dropdownSelection:string="No.Doesn't Require Delivery";
-shippingOptions:any[]=[
- {title:'Yes,Require Shipping.', value:true},
- {title: "No.Doesn't Require Delivery",value:false}
+// weightDropDownSelection:string='KG';
+ActiveDropDownSelection:string='Active';
+chargeTaxDropDownSelection:string='Active';
+priceAffecringDropDownSelection:string='yes'
+// dropdownSelection:string="No.Doesn't Require Delivery";
+// shippingOptions:any[]=[
+//  {title:'Yes,Require Shipping.', value:true},
+//  {title: "No.Doesn't Require Delivery",value:false}
+// ]
+ActiveOptions:any[]=[
+{title:'Active', value:true},
+ {title: "Inactive",value:false}
 ]
-weightOptions:any[]=[
-{title:'KG', value:'KG'},
- {title: "gram",value:'gram'}
+ChargeTax:any[]=[
+{title:'Charge', value:true},
+ {title: "Uncharge",value:false}
 ]
+PriceAffecting:any[]=[
+{title:'yes', value:true},
+ {title: "no",value:false}
+]
+// weightOptions:any[]=[
+// {title:'KG', value:'KG'},
+//  {title: "gram",value:'gram'}
+// ]
 
 
 constructor(){
@@ -40,45 +64,121 @@ constructor(){
   });
 }
 closeProductPopscreen(){
-this.updated.emit()
+this.close.emit()
 }
 saveProductDetails(){
   this.getProductInfo();
 }
 
 displayProductInfo(){
-  const { weight, productCostPrice, salePrice, salePriceEndDate,weightUnit,barcode,requireShipping} = this.product() ?? {};
-  this.productWeight.value = weight?.toString() ?? '';
-  this.productCostPrice.value = productCostPrice?.toString() ?? '';
+  // const { weight, productCostPrice, salePrice, salePriceEndDate,weightUnit,barcode,requireShipping} = this.product() ?? {};
+  const { sku,comparePrice,barcode,isActive,isPriceAffecting,descriptionEn,descriptionAr,chargeTax,tax,vatValue} = this.product() ?? {};
+  // this.productWeight.value = weight?.toString() ?? '';
+  // this.productCostPrice.value = productCostPrice?.toString() ?? '';
   this.barcode.value = barcode ?? null;
-  this.salePriceEndDate.value = (salePriceEndDate && salePrice)?salePriceEndDate : '';
-  if(weightUnit)
-    this.weightDropDownSelection =weightUnit;
-   if(salePrice)
-    this.salePrice.value = salePrice?.toString()
-    this.dropdownSelection =requireShipping?'Yes,Require Shipping.':"No.Doesn't Require Delivery";
+  this.SKU.value = sku ?? null;
+  this.descriptionEn.value = descriptionEn ?? '';
+  this.descriptionAr.value = descriptionAr ?? '';
+  this.productComparePrice.value = comparePrice?.toString() ?? '';
+  this.ActiveDropDownSelection= isActive ? 'Active' : 'Inactive';
+  this.chargeTaxDropDownSelection = chargeTax ? 'Charge' : 'Uncharge';
+  this.priceAffecringDropDownSelection = isPriceAffecting ? 'yes' : 'no';
+  this.tax.value = tax?.toString() ?? '';
+  this.vatValue.value = vatValue?.toString() ?? '';
+  // this.salePriceEndDate.value = (salePriceEndDate && salePrice)?salePriceEndDate : '';
+  // if(weightUnit)
+  //   this.weightDropDownSelection =weightUnit;
+  //  if(salePrice)
+  //   this.salePrice.value = salePrice?.toString()
+  //   this.dropdownSelection =requireShipping?'Yes,Require Shipping.':"No.Doesn't Require Delivery";
   }
 
 changeSelect(method:any,key:string){
   this.product()[key]=method.value;
-  if(key == 'weightUnit') this.weightDropDownSelection=method.value;
+  // if(key == 'weightUnit') this.weightDropDownSelection=method.value;
+   if(key == 'isActive') this.ActiveDropDownSelection=method.title;
+    if(key == 'chargeTax') this.chargeTaxDropDownSelection=method.title;
+    if(key == 'isPriceAffecting') this.priceAffecringDropDownSelection=method.title;
 }
 
+// getProductInfo(): void {
+//   const weight = parseFloat(this.productWeight.value);
+//   const costPrice = parseFloat(this.productCostPrice.value);
+//   const salePrice = parseFloat(this.salePrice.value);
+//   const salePriceEndDate =this.salePriceEndDate.value? new Date(this.salePriceEndDate.value).toISOString().split('T')[0]:'';
+//   const barcode = this.barcode.value;
+//   const id=this.product().id
+//   this.__ProductService.currentProduct.set({
+//     ...this.product(),
+//     weight,
+//     weightUnit: weight ? this.weightDropDownSelection : '',
+//     productCostPrice: costPrice,
+//     salePrice,
+//     barcode,
+//     salePriceEndDate
+//   });
+//   // this.updated.emit(true);
+//   if(id)
+//   this.__ProductService.patchProduct({
+//     id,
+//     weight,
+//     weightUnit: weight ? this.weightDropDownSelection : '',
+//     productCostPrice: costPrice,
+//     salePrice,
+//     barcode,
+//     salePriceEndDate
+//   }).subscribe({
+//     next:()=> this.close.emit(true)
+//   });
+
+
+// }
 getProductInfo(): void {
-  const weight = parseFloat(this.productWeight.value);
-  const costPrice = parseFloat(this.productCostPrice.value);
-  const salePrice = parseFloat(this.salePrice.value);
-  const salePriceEndDate =this.salePriceEndDate.value? new Date(this.salePriceEndDate.value).toISOString().split('T')[0]:'';
+  const sku=this.SKU.value;
+  const descriptionEn = this.descriptionEn.value;
+  const descriptionAr = this.descriptionAr.value;
+  const tax = parseFloat(this.tax.value);
+  const vatValue = parseFloat(this.vatValue.value);
+  const comparePrice= parseFloat(this.productComparePrice.value);
+  const isActive = this.ActiveDropDownSelection === 'Active';
+  const chargeTax = this.chargeTaxDropDownSelection === 'Charge';
+  const isPriceAffecting = this.priceAffecringDropDownSelection === 'yes';
+  // const costPrice = parseFloat(this.productCostPrice.value);
   const barcode = this.barcode.value;
+  const id=this.product().id
   this.__ProductService.currentProduct.set({
     ...this.product(),
-    weight,
-    weightUnit: weight ? this.weightDropDownSelection : '',
-    productCostPrice: costPrice,
-    salePrice,
+    sku,
+    descriptionEn,
+    descriptionAr,
+    tax,
     barcode,
-    salePriceEndDate
+    vatValue,
+    comparePrice,
+    isActive,
+    chargeTax,
+    isPriceAffecting
   });
-  this.updated.emit(true);
+  // this.updated.emit(true);
+  if(id)
+     this.__ProductService.updateProduct(this.__ProductService.currentProduct()).subscribe({
+    next:()=> this.close.emit(true)
+  });
+  // this.__ProductService.patchProduct({
+  //   id,
+  //    descriptionEn,
+  //   descriptionAr,
+  //   tax,
+  //   barcode,
+  //   vatValue,
+  //   comparePrice,
+  //   isActive,
+  //   chargeTax,
+  //   isPriceAffecting
+  // }).subscribe({
+  //   next:()=> this.close.emit(true)
+  // });
+
+
 }
 }
