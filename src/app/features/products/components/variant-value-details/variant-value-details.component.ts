247cf4dbb3d8b0c2d43acb7e739d7d4f3f22fafe
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-06-20 12:06:57 +0300

    update varaint value details to match new fields in product variant master api

--- src/app/features/products/components/variant-value-details/variant-value-details.component.ts
+++ src/app/features/products/components/variant-value-details/variant-value-details.component.ts
@@ -3,12 +3,6 @@ import {
   Component,EventEmitter, Input, Output,QueryList,ViewChildren,AfterViewInit,OnDestroy,signal,computed,effect} from '@angular/core';
 import { Subject } from 'rxjs';
 import { InputComponent } from '../../../../shared/components/input/input.component';
-
-// interface VariantValueDetailsInterface{
-// quantity:string,
-// displayedVariants:any[],
-// index:number
-// }
 interface VariantValueDetailsInterface{
 quantity:number,
 displayedVariants:any[],
@@ -21,15 +15,19 @@ index:number
   standalone:false
 })
 export class VariantValueDetailsComponent implements AfterViewInit, OnDestroy {
-  private destroy$ = new Subject<void>();
+private destroy$ = new Subject<void>();
 
-  // private _inputData = signal<VariantValueDetailsInterface>({
-  //   quantity: '0',
-  //   displayedVariants: [],
-  //   index: -1,
-  // });
-
-    private _inputData = signal<VariantValueDetailsInterface>({
+chargeTaxDropDownSelection:string='Charge';
+priceAffecringDropDownSelection:string='yes';
+ChargeTax:any[]=[
+{title:'Charge', value:true},
+ {title: "Uncharge",value:false}
+]
+PriceAffecting:any[]=[
+{title:'yes', value:true},
+ {title: "no",value:false}
+]
+private _inputData = signal<VariantValueDetailsInterface>({
     quantity: 0,
     displayedVariants: [],
     index: -1,
@@ -57,17 +55,22 @@ ngAfterViewInit() {
   this.tryDisplayVariantDetails();
 }
 
+changeSelect(method:any,key:string){
+    if(key == 'chargeTax') this.chargeTaxDropDownSelection=method.title;
+    if(key == 'isPriceAffecting') this.priceAffecringDropDownSelection=method.title;
+}
+
 readonly _effect = effect(() => {
   if (!this.isViewInit) return;
   this.tryDisplayVariantDetails();
 });
 
-  private tryDisplayVariantDetails() {
+private tryDisplayVariantDetails() {
     if (!this.variantDetail() || !this.inputComponentRefs?.length) return;
     this.displayVariantDetails();
   }
 
-  displayVariantDetails() {
+displayVariantDetails() {
     const detail = this.variantDetail();
     if (!detail) return;
 
@@ -83,15 +86,8 @@ readonly _effect = effect(() => {
     });
   }
 
-  // updateQuantity(newQuantity: string) {
-  //   const updated = this._inputData();
-  //   this._inputData.set({
-  //     ...updated,
-  //     quantity: newQuantity,
-  //   });
-  //   this.variantProductDetaills.emit(newQuantity);
-  // }
-    updateQuantity(newQuantity: number) {
+
+  updateQuantity(newQuantity: number) {
     const updated = this._inputData();
     this._inputData.set({
       ...updated,
@@ -110,11 +106,42 @@ readonly _effect = effect(() => {
         variantDetailsValue[id] = value;
       }
     });
-
     variantDetailsValue['quantity'] = this._inputData().quantity;
+    variantDetailsValue['chargeTax'] = this.chargeTaxDropDownSelection == 'Charge' ;
+    variantDetailsValue['isPriceAffecting'] = this.priceAffecringDropDownSelection == 'yes' ;
+    console.log('getDtails',variantDetailsValue)
     return variantDetailsValue;
   }
 
+  recalculate() {
+  const inputs = this.inputComponentRefs.toArray();
+  const priceComp = inputs[0];
+  const costComp = inputs[1];
+  const profitComp = inputs[3];
+  const marginComp = inputs[4];
+
+  const price = Number(priceComp?.value);
+  const costPrice = Number(costComp?.value);
+
+  const isValidPrice = !isNaN(price) && price > 0;
+  const isValidCost = !isNaN(costPrice);
+
+  if (isValidPrice && isValidCost && profitComp && marginComp) {
+    if (costPrice <= price) {
+      const profit = price - costPrice;
+      const margin = (profit / price) * 100;
+
+      profitComp.value = profit.toFixed(2);
+      marginComp.value = margin.toFixed(1);
+    } else {
+      profitComp.value = '0.00';
+      marginComp.value = '0.0';
+    }
+  } else {
+    if (profitComp) profitComp.value = '';
+    if (marginComp) marginComp.value = '';
+  }
+}
    ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
