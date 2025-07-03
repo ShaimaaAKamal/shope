7f5dc764f6462c2813df3802ca5343cafd1363ee
ShaimaaAKamal <shaimaakamal039@gmail.com>
2025-07-03 19:04:02 +0300

    add some missing traanslation messages

--- src/app/shared/components/page-controls/page-controls.component.ts
+++ src/app/shared/components/page-controls/page-controls.component.ts
@@ -1,58 +1,6 @@
-// import { Component, EventEmitter, Input, Output } from '@angular/core';
-// import { Router } from '@angular/router';
-
-// interface PageControlsInterface {
-//   route: string;
-//   type: string;
-//   items: any[];
-//   sortKey: string;
-// }
-// @Component({
-//   selector: 'app-page-controls',
-//   standalone: false,
-//   templateUrl: './page-controls.component.html',
-//   styleUrl: './page-controls.component.scss'
-// })
-// export class PageControlsComponent {
-//   @Input() PageControlsComponentData:PageControlsInterface={
-//   route: '',
-//   type: '',
-//   items: [],
-//   sortKey: ''
-//   }
-//   @Output() displayedItems=new EventEmitter<any[]>()
-//   @Output() deleteEvent=new EventEmitter<boolean>()
-//   @Output() addNewVar=new EventEmitter<boolean>()
-
-//   constructor(private __Router:Router){}
-
-// AddNew(){
-//     switch(this.PageControlsComponentData.type){
-//       case 'product': this.addNewVar.emit(true); break;
-//       case 'order':   this.__Router.navigateByUrl(this.PageControlsComponentData.route); break;
-//       default:break;
-
-//     }
-//   }
-
-// sort(){
-// this.PageControlsComponentData.items.sort((a, b) => a[this.PageControlsComponentData.sortKey] - b[this.PageControlsComponentData.sortKey]);
-// this.displayedItems.emit(this.PageControlsComponentData.items);
-// }
-// deleteSelected(){
-//   this.deleteEvent.emit(true)
-// }
-// }
-
-
-
-
-
-
 import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { ServiceInterface } from '../../../Interfaces/service-interface';
-import { FormBuilder } from '@angular/forms';
 
 interface PageControlsInterface {
   route: string;
